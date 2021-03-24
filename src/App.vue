98739775bec6836aa70ca2f73a479b5bdc667ac6
Администратор <12345kolt@gmail.com>
2021-03-24 19:39:03 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+ <div id="App">
+   <b-button>Button</b-button>
+   <b-button variant="danger">Button</b-button>
+   <b-button variant="success">Button</b-button>
+   <b-button variant="outline-primary">Button</b-button>
+ </div>
+</template>
+
+<script>
+
+export default {
+  name: "App",
+  components: {
+
+  },
+};
+</script>
+
+<style>
+
+</style>

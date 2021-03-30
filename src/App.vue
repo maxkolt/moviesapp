49aaacf90e40e20617638266764e80cc77b77c78
Администратор <12345kolt@gmail.com>
2021-03-30 21:17:28 +0300

    Проект moviesapp

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,41 @@
 <template>
   <div id="app">
-    <b-button>Button</b-button>
-    <b-button variant="danger">Button</b-button>
-    <b-button variant="success">Button</b-button>
-    <b-button variant="outline-primary">Button</b-button>
+    <PosterBg :poster="posterBg" />
+    <MoviesList :list="moviesList" @changePoster="onChangePoster" />
   </div>
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import { mapActions, mapGetters } from "vuex";
+import MoviesList from "@/components/MoviesList";
+import PosterBg from "@/components/PosterBg";
 
 export default {
   name: "app",
-  components: {},
-  mounted() {
-    this.fetchMovies();
+  components: {
+    MoviesList,
+    PosterBg
+  },
+  data: () => ({
+    posterBg: ""
+  }),
+  computed: {
+    ...mapGetters("movies", ["moviesList"])
   },
   methods: {
-    ...mapActions("movies", ["fetchMovies"])
+    ...mapActions("movies", ["fetchMovies"]),
+    onChangePoster(poster) {
+      this.posterBg = poster;
+    }
   }
 };
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  position: relative;
 }
 </style>
